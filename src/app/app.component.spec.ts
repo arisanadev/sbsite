07ba284aff1258fe59d560aff829f578ec4f62c8
Ariê Furtado <arie@nanuven.com>
2019-01-27 22:21:46 -0200

    Rename Angular Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'teste1'`, () => {
+  it(`should have as title 'sbsite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('teste1');
+    expect(app.title).toEqual('sbsite');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to teste1!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sbsite!');
   });
 });
